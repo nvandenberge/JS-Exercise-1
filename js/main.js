f69e292eb7eed092f54126bd37ee05e6925d41a8
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-08 20:02:41 -0500

    added alert if field is empty

--- js/main.js
+++ js/main.js
@@ -3,43 +3,39 @@ const input = document.querySelector('input')
 input.addEventListener('keypress', eventHandler)
 
   function eventHandler (event) {
+    const p = document.createElement('p');
+    const form = document.querySelector('form');
+
     if (event.which === 13) {
-      const p = document.createElement('p');
+      if  (input.value.length === 0) {
+        alert('Enter your name')
+      }
+      else {
       p.innerText = `Hello ${input.value}, nice to meet you!`;
-      const form = document.querySelector('form')
       form.append(p);
       event.preventDefault();
       document.getElementById('yourName').value = "";
     }
-
   }
+}
 
 
 
 
-
-
-  // const form = document.querySelector('form');
-  // const name = document.querySelector('input');
-  // const span = document.querySelector('span');
-  // const newP = document.createElement('p');
+  // const input = document.querySelector('input')
   //
-  // function eventHandler (event) {
-  //   let yourName = name;
-  //   if (event.which == 13) {
-  //       return  newP.innerText = name.value
+  // input.addEventListener('keypress', eventHandler)
+  //
+  //   function eventHandler (event) {
+  //     if (event.which === 13) {
+  //       const p = document.createElement('p');
+  //       p.innerText = `Hello ${input.value}, nice to meet you!`;
+  //       const form = document.querySelector('form')
+  //       form.append(p);
+  //       event.preventDefault();
+  //       document.getElementById('yourName').value = "";
+  //     }
   //   }
-  //   else {
-  //     alert('You must press the Enter key')
-  //   }
-  // }
-
-
-
-
-
-// input.addEventListener('submit', function (event) {
-//   const newP = document.createElement('p')
-//   newP.textContent = `Hello ${name}, nice to meet you!`
-
-// })
+  //
+  //
+  //

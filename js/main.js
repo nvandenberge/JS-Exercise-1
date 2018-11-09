638b088653cabfc06ee452645163bfd24b2a9230
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-09 16:03:48 -0500

    added prompt when input field is empty // will insert prompt value to string if value is higher than 0 // no value in prompt will generate error

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 const input = document.querySelector('input')
-
 input.addEventListener('keypress', eventHandler)
 
-  function eventHandler (event) {
-    const p = document.createElement('p');
-    const form = document.querySelector('form');
+function eventHandler(event) {
+  const p = document.createElement('p');
+  const form = document.querySelector('form');
 
-    if (event.which === 13) {
-      if  (input.value.length === 0) {
-        alert('Enter your name')
+  if (event.which === 13) {
+    if (input.value.length === 0) {
+      const person = prompt('Please enter your name:');
+      if (person.length > 0) {
+        p.innerText = `Hello ${person}, nice to meet you!`;
+        form.append(p);
+        event.preventDefault();
+      } else if (person.length === 0) {
+        alert("You did not enter your name!")
+        p.innerText = 'ERROR: You did not enter your name!';
+        form.append(p);
+        event.preventDefault();
       }
-      else {
+    } else {
       p.innerText = `Hello ${input.value}, nice to meet you!`;
       form.append(p);
       event.preventDefault();
@@ -19,23 +27,59 @@ input.addEventListener('keypress', eventHandler)
   }
 }
 
+//          WORKING CODE WITH PROMPT AND OUTPUT
+
+// const input = document.querySelector('input')
+// input.addEventListener('keypress', eventHandler)
+//
+// function eventHandler(event) {
+//   const p = document.createElement('p');
+//   const form = document.querySelector('form');
+//
+//   if (event.which === 13) {
+//     if (input.value.length === 0) {
+//       const person = prompt('Please enter your name:');
+//       if (person.length > 0) {
+//         p.innerText = `Hello ${person}, nice to meet you!`;
+//         form.append(p);
+//         event.preventDefault();
+//       } else if (person.length === 0) {
+//         alert("You did not enter your name!")
+//         p.innerText = 'ERROR: You did not enter your name!';
+//         form.append(p);
+//         event.preventDefault();
+//       }
+//     } else {
+//       p.innerText = `Hello ${input.value}, nice to meet you!`;
+//       form.append(p);
+//       event.preventDefault();
+//       document.getElementById('yourName').value = "";
+//     }
+//   }
+// }
 
 
 
-  // const input = document.querySelector('input')
-  //
-  // input.addEventListener('keypress', eventHandler)
-  //
-  //   function eventHandler (event) {
-  //     if (event.which === 13) {
-  //       const p = document.createElement('p');
-  //       p.innerText = `Hello ${input.value}, nice to meet you!`;
-  //       const form = document.querySelector('form')
-  //       form.append(p);
-  //       event.preventDefault();
-  //       document.getElementById('yourName').value = "";
-  //     }
-  //   }
-  //
-  //
-  //
+//      ORIGINAL WORKING CODE BELOW (DO NOT TOUCH)
+
+// const input = document.querySelector('input')
+//
+// input.addEventListener('keypress', eventHandler)
+//
+//   function eventHandler (event) {
+//     const p = document.createElement('p');
+//     const form = document.querySelector('form');
+//
+//     if (event.which === 13) {
+//       if  (input.value.length === 0) {
+//         alert('Enter your name')
+//       }
+//       else {
+//       p.innerText = `Hello ${input.value}, nice to meet you!`;
+//       form.append(p);
+//       event.preventDefault();
+//       document.getElementById('yourName').value = "";
+//     }
+//   }
+// }
+//
